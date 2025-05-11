70318cb73f18cd8bde1ecad796e1fbb8378a8fd2
Mian M Owais <mianowais980@gmail.com>
2025-05-12 01:36:37 +0500

    Initial commit

--- owaisss/client/src/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import documentReducer from './features/documents/documentSlice';
-
-export const store = configureStore({
-  reducer: {
-    documents: documentReducer,
-  },
-});
